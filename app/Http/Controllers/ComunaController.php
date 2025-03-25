552a31df50e1b47ac71e5aa916e2a6be01968c60
Diana Perez <dianaperezc10@gmail.com>
2025-03-25 15:49:21 -0500

    Commune Delete

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -8,17 +8,32 @@ use Illuminate\Support\Facades\DB;
 
 class ComunaController extends Controller
 {
+    /**
+ * Display a listing of the resource.
+ *
+ * @return \Illuminate\Http\Response
+ */
     public function index()
     {
-        $comunas = Comuna::all();
-        return view('comuna.index', ['comunas' => $comunas]);
+    //
+    //$comunas = Comuna::all();
+    $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+        ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+        ->get();
+
+    return view('comuna.index', ['comunas' => $comunas]);
     }
     /**
      * Show the form for creating a new resource.
      */
     public function create()
     {
-        //
+        $municipios = DB::table('tb_municipio')
+        ->orderBy('muni_nomb')
+        ->get();
+
+    return view('comuna.new', ['municipios' => $municipios]);
     }
 
     /**
@@ -26,7 +41,19 @@ class ComunaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $comuna = new Comuna();
+        // $comuna->comu_codi = $request->id;
+        // El código de comuna es auto incremental
+        $comuna->comu_nomb = $request->name;
+        $comuna->muni_codi = $request->code;
+        $comuna->save();
+
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+        ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+        ->get();
+
+        return view('comuna.index', ['comunas' => $comunas]);
     }
 
     /**
